e7fc14700f39d22898952b5a60e2d90d4a12381b
Danil <dynuli01@mail.ru>
2024-10-18 22:30:02 +0700

    Динамическое выделение памяти

--- Лабораторная №2/Header.h
+++ Лабораторная №2/Header.h
@@ -5,49 +5,56 @@ struct Client {
     char address[100];
 };
 
-// Структура для хранения данных о категории товара
-struct Category {
-    char name[50];   // Название категории ("Корм", "Игрушки", "Аксессуары")
-};
-
 // Структура для хранения данных о поставщике
 struct Supplier {
+    struct Product *products;
     char name[50];  // Название поставщика
     char address[100];  // Адрес поставщика
     char phone[20];  // Телефон поставщика
+};
+
+// Структура для хранения данных о категории товара
+struct Category {
+    char name[50];   // Название категории ("Корм", "Игрушки", "Аксессуары")
+    struct Product *products;
 };
 
 // Структура для хранения информации о товаре
 struct Product {
     char name[50];
     int price;
-    int kolvo;
+    int count;
+    int category_id;
+    int supplier_id;
 };
 
 // Структура для хранения информации о заказе
 struct Order {
     int client_id;
-    struct Product products[100];
+    struct Product *products;
     int num_products;
     int total_price;
+    int status_order;
+};
+
+// Структура для хранения информации о количестве товара
+struct Storehouse {
+    struct Product *products;
 };
 
 // Структура для хранения истории заказов
 struct OrderHistory {
-    struct Order orders[100]; // Массив заказов
+    struct Order *orders; // Массив заказов
     int orderCount;
-    struct Product products[100];
-    int num_products;
 };
 
 // Структура для хранения информации о магазине
 struct Store {
-    struct Client clients[100];
+    struct Client *clients;
     int num_clients;
-    struct Category categories[20];
+    struct Category *categories;
     int num_categories;
-    struct Supplier suppliers[50];
-    int num_supplier;
-    struct Product products[100];
+    struct Supplier *suppliers;
+    int num_suppliers;
     int num_products;
 };
--- Лабораторная №2/Лабораторная №2.cpp
+++ Лабораторная №2/Лабораторная №2.cpp
@@ -6,7 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
+#include <malloc.h>
+#include <Windows.h>
 #include "Header.h"
+
 
 // Функция для добавления нового клиента
 void add_client(struct Store* store) {
@@ -18,7 +21,10 @@ void add_client(struct Store* store) {
     printf("Введите адрес клиента: ");
     scanf("%s", new_client.address);
     // Добавление нового клиента в массив клиентов
-    store->clients[store->num_clients] = new_client;
+    store->clients = (Client*)realloc(store->clients, (store->num_clients + 1) * sizeof(struct Client));
+    strcpy(store->clients[store->num_clients].name, new_client.name);
+    strcpy(store->clients[store->num_clients].phone, new_client.phone);
+    strcpy(store->clients[store->num_clients].address, new_client.address);
     store->num_clients++;
     printf("Клиент добавлен!\n");
 }
@@ -29,7 +35,8 @@ void add_category(struct Store* store) {
     printf("Введите название категории: ");
     scanf("%s", new_category.name);
     // Добавление новой категории в массив категорий
-    store->categories[store->num_categories] = new_category;
+    store->categories = (Category*)realloc(store->categories, (store->num_categories + 1) * sizeof(struct Category));
+    strcpy(store->categories[store->num_categories].name, new_category.name);
     store->num_categories++;
     printf("Категория добавлена!\n");
 }
@@ -44,29 +51,34 @@ void add_supplier(struct Store* store) {
     printf("Введите телефон поставщика: ");
     scanf("%s", new_supplier.phone);
     // Добавление нового поставщика в массив поставщиков
-    store->suppliers[store->num_supplier] = new_supplier;
-    store->num_supplier++;
+    store->suppliers = (Supplier*)realloc(store->suppliers, (store->num_suppliers + 1) * sizeof(struct Supplier));
+    store->suppliers[store->num_suppliers] = new_supplier;
+    store->num_suppliers++;
     printf("Поставщик добавлен!\n");
 }
 
 // Функция для добавления нового товара
-void add_product(struct Store* store) {
+void add_product(struct Store* store, struct Storehouse* storehouse) {
+    if (store->num_products==0) storehouse->products = (Product*)malloc((store->num_products + 1) * sizeof(struct Product));
     struct Product new_product;
-    int catogory_id, supplier_id;
+    int category_id, supplier_id;
     printf("Введите ID категории: ");
-    scanf("%d", &catogory_id);
-    if (catogory_id > 0 && catogory_id <= store->num_categories) {
+    scanf("%d", &category_id);
+    if (category_id > 0 && category_id <= store->num_categories) {
         printf("Введите ID поставщика: ");
         scanf("%d", &supplier_id);
-        if (supplier_id > 0 && supplier_id <= store->num_supplier) {
+        if (supplier_id > 0 && supplier_id <= store->num_suppliers) {
             printf("Введите название товара: ");
             scanf("%s", new_product.name);
             printf("Введите цену товара: ");
             scanf("%d", &new_product.price);
             printf("Введите количество товара: ");
-            scanf("%d", &new_product.kolvo);
+            scanf("%d", &new_product.count);
+            new_product.category_id = category_id  - 1;
+            new_product.supplier_id = supplier_id - 1;
             // Добавление нового товара в массив товаров
-            store->products[store->num_products] = new_product;
+            storehouse->products = (Product*)realloc(storehouse->products, (store->num_products + 1) * sizeof(struct Product));
+            storehouse->products[store->num_products] = new_product;
             store->num_products++;
             printf("Товар добавлен!\n");
         }
@@ -76,26 +88,32 @@ void add_product(struct Store* store) {
 }
 
 // Функция для создания нового заказа
-void create_order(struct OrderHistory* history) {
+void create_order(struct Store* store, struct OrderHistory* history, struct Storehouse* storehouse) {
+    int client_id, product_id, volume;
     struct Order new_order;
-    int client_id, product_id, kolvo;
-    printf("Введите ID клиента: ");
-    scanf("%d", &client_id);
+    if (history->orderCount == 0) history->orders = (Order*)malloc((history->orderCount + 1) * sizeof(struct Order));
+    do {
+        printf("Введите ID клиента: ");
+        scanf("%d", &client_id);
+    } while (client_id < 0 || client_id > store->num_clients);
     new_order.client_id = client_id;
     new_order.num_products = 0;
     new_order.total_price = 0;
     printf("Введите ID товара (или 0 для завершения): ");
     scanf("%d", &product_id);
     while (product_id != 0) {
-        if (product_id > 0 && product_id <= history->num_products) {
+        if (product_id > 0 && product_id <= store->num_products) {
             printf("Введите количество товара: ");
-            scanf("%d", &kolvo);
-            if (kolvo > 0 && kolvo <= history->products[product_id - 1].kolvo) {
-                new_order.products[new_order.num_products] = history->products[product_id - 1];
-                new_order.products[new_order.num_products].kolvo = kolvo;
-                new_order.total_price += new_order.products[new_order.num_products].price * kolvo;
+            scanf("%d", &volume);
+            if (volume > 0 && volume <= storehouse->products[product_id - 1].count) {
+                if (new_order.num_products==0) new_order.products = (Product*)malloc((new_order.num_products + 1) * sizeof(struct Product));
+                new_order.products = (Product*)realloc(new_order.products, (new_order.num_products + 1) * sizeof(struct Product));
+                new_order.products[new_order.num_products] = storehouse->products[product_id - 1];
+                new_order.products[new_order.num_products].count = volume;
+                new_order.total_price += new_order.products[new_order.num_products].price * volume;
+                new_order.status_order = 0;
                 new_order.num_products++;
-                history->products[product_id - 1].kolvo -= kolvo;
+                storehouse->products[product_id - 1].count -= volume;
             }
             else {
                 printf("Неверное количество товара!\n");
@@ -104,11 +122,12 @@ void create_order(struct OrderHistory* history) {
         else {
             printf("Неверный ID товара!\n");
         }
-        printf("Введите ID товара (или 0 для завершения): ");
+        printf("Введите ID нового товара (или 0 для завершения): ");
         scanf("%d", &product_id);
     }
-
     // Добавление нового заказа в массив заказов
+    history->orders = (Order*)realloc(history->orders, (history->orderCount + 1) * sizeof(struct Order));
+    history->orders[history->orderCount].products = (Product*)malloc((new_order.num_products + 1) * sizeof(struct Product));
     history->orders[history->orderCount] = new_order;
     history->orderCount++;
     printf("Заказ создан!\n");
@@ -139,7 +158,7 @@ void list_category(struct Store* store) {
 // Функция для вывода списка поставщиков
 void list_supplier(struct Store* store) {
     printf("Список поставщиков:\n");
-    for (int i = 0; i < store->num_supplier; i++) {
+    for (int i = 0; i < store->num_suppliers; i++) {
         printf("ID: %d\n", i + 1);
         printf("Название поставщика: %s\n", store->suppliers[i].name);
         printf("Адрес поставщика: %s\n", store->suppliers[i].address);
@@ -156,7 +175,7 @@ void list_orders(struct OrderHistory* history) {
         printf("ID клиента: %d\n", history->orders[i].client_id);
         printf("Товары:\n");
         for (int j = 0; j < history->orders[i].num_products; j++) {
-            printf("%d. %s (количество: %d)\n", j + 1, history->orders[i].products[j].name, history->orders[i].products[j].kolvo);
+            printf("%d. %s (количество: %d)\n", j + 1, history->orders[i].products[j].name, history->orders[i].products[j].count);
         }
         printf("Общая стоимость: %d\n", history->orders[i].total_price);
         printf("\n");
@@ -164,15 +183,15 @@ void list_orders(struct OrderHistory* history) {
 }
 
 // Функция для вывода списка товаров
-void list_products(struct Store* store) {
+void list_products(struct Store* store, struct Storehouse* storehouse) {
     printf("Список товаров:\n");
     for (int i = 0; i < store->num_products; i++) {
         printf("ID: %d\n", i + 1);
-        printf("Категория: %s\n", store->categories[i].name);
-        printf("Поставщик: %s\n", store->suppliers[i].name);
-        printf("Название: %s\n", store->products[i].name);
-        printf("Цена: %d\n", store->products[i].price);
-        printf("Количество: %d\n", store->products[i].kolvo);
+        printf("Категория: %s\n", store->categories[storehouse->products[i].category_id].name);
+        printf("Поставщик: %s\n", store->suppliers[storehouse->products[i].supplier_id].name);
+        printf("Название: %s\n", storehouse->products[i].name);
+        printf("Цена: %d\n", storehouse->products[i].price);
+        printf("Количество: %d\n", storehouse->products[i].count);
         printf("\n");
     }
 }
@@ -196,15 +215,18 @@ void outmenu() {
 
 int main()
 {
-    setlocale(LC_ALL, "RUS");
-    system("chcp 1251");
-    struct Store store;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(0, "Rus");
+    struct Store store = { NULL, 0, NULL, 0, NULL, 0, 0};
     store.num_clients = 0;
     store.num_categories = 0;
-    store.num_supplier = 0;
+    store.num_suppliers = 0;
     store.num_products = 0;
+    struct Order order;
+    struct Storehouse storehouse;
     struct OrderHistory history;
-    history.num_products = 0;
+    history.orderCount = 0;
     int choice;
     do {
         outmenu();
@@ -220,10 +242,10 @@ int main()
             add_supplier(&store);
             break;
         case 4:
-            add_product(&store);
+            add_product(&store, &storehouse);
             break;
         case 5:
-            create_order(&history);
+            create_order(&store, &history, &storehouse);
             break;
         case 6:
             list_clients(&store);
@@ -235,7 +257,7 @@ int main()
             list_supplier(&store);
             break;
         case 9:
-            list_products(&store);
+            list_products(&store, &storehouse);
             break;
         case 10:
             list_orders(&history);
